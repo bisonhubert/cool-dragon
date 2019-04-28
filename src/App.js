e2fd943246e9a15379b565d69b77594f479beb95
Bison Hubert <bisonhubert@gmail.com>
2019-04-27 17:08:26 -0700

    Create app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import dragon_1 from './images/cool-dragon-2.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>omg cool dragon</h1>
+        <img src={dragon_1} className="App-logo" alt="logo" />
       </header>
     </div>
   );
