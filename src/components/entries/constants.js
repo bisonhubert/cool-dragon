8bc48f068f8db9164ce9e43f88594d91deeb2d0f
Bison Hubert <bisonhubert@gmail.com>
2019-04-29 18:40:26 -0700

    Add constants

---
+++ src/components/entries/constants.js
@@ -0,0 +1,58 @@
+// Character List
+export const ALYS = 'Alys Karstark'
+export const ARYA = 'Arya Stark'
+export const BERIC = 'Beric Dondarrion'
+export const BRAN = 'Bran Stark'
+export const BRONN = 'Bronn'
+export const BRIENNE = 'Brienne of Tarth'
+export const CERSEI = 'Cersei Lannister'
+export const DROGON = 'Drogon'
+export const ELLARIA = 'Ellaria Sand'
+export const EDD = 'Eddison Tollett'
+export const DAARIO = 'Daario Naharis'
+export const DAENERYS = 'Daenerys Targaryen'
+export const DAVOS = 'Davos Seaworthy'
+export const EURON = 'Euron Greyjoy'
+export const GENDRY = 'Gendry'
+export const GHOST = 'Ghost'
+export const GILLY = 'Gilly'
+export const GREY_WORM = 'Grey Worm'
+export const HARRY = 'Harry Strickland'
+export const HOT_PIE = 'Hot Pie'
+export const HOUND = 'The Hound'
+export const JAMIE = 'Jamie Lannister'
+export const JAQEN = "Jaqen H'ghar"
+export const JON_SNOW = 'Jon Snow'
+export const JORAH = 'Jorah Mormont'
+export const LYANNA = 'Lyanna Mormont'
+export const MEERA = 'Meera Reed'
+export const MELISANDRE = 'Melisandre'
+export const MISSANDEI = 'Missandei'
+export const MOUNTAIN = 'The Mountain'
+export const NIGHT_KING = 'The Night King'
+export const PODRICK = 'Podrick Payne'
+export const QHONO = 'Qhono'
+export const QYBURN = 'Qyburn'
+export const RHAEGAL = 'Rhaegal'
+export const ROBIN = 'Robin Arryn'
+export const SAM = 'Samwell Tarly'
+export const SAM_JR = 'Sam Jr.'
+export const SANSA = 'Sansa Stark'
+export const THEON = 'Theon Greyjoy'
+export const TORMUND = 'Tormund Giantsbane'
+export const TYRION = 'Tyrion Lannister'
+export const VARYS = 'Varys'
+export const VISERION = 'Viserion'
+export const WOLKAN = 'Maester Wolkan'
+export const YARA = 'Yara Greyjoy'
+export const YOHN = 'Yohn Royce'
+
+
+// Bonus Questions
+export const BONUS_1 = ARYA
+export const BONUS_2 = undefined
+
+
+// Leaderboard Types
+export const BRAVA = 'brava'
+export const HOMIES = 'homies'
