7cc393cc3df08389c80d5d71cdf974a2e52ea556
Bison Hubert <bisonhubert@gmail.com>
2019-04-30 19:50:11 -0700

    Get current master entry

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import { bravaEntries, homieEntries } from './components/entries/entryData';
+// import { bravaEntries, homieEntries } from './components/entries/entryData';
+import { bravaEntries, homieEntries, masterEntries, playerEntries } from './components/entries/entryData';
+import { BRAVA, HOMIES } from './components/entries/constants.js';
 import Leaderboard from './components/leaderboard/Leaderboard';
 
 import nightKingKiller from './images/arya-night-king-2.gif'
@@ -9,22 +11,30 @@ import nightKingKiller from './images/arya-night-king-2.gif'
 // import icon from './images/cool-dragon-2.gif';
 
 function App() {
+  // const getBravaEntries = () => {
+  //   // filter playerEntries for leaderboard = 'BRAVA'
+  // }
+
+  // const getHomieEntries = () => {
+  //   // filter playerEntries for leaderboard = 'HOMIE'
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
         <article className="w-100p">
           <h2>Brava Leaderboard</h2>
           <h3>Pot: $75</h3>
-          <Leaderboard entries={bravaEntries} />
+          <Leaderboard entries={bravaEntries} masterEntries={masterEntries} playerEntries={playerEntries} leaderboard={BRAVA} />
         </article>
         <article className="w-100p">
           <h2>Homies Leaderboard</h2>
           <h3>Pot: $30</h3>
-          <Leaderboard entries={homieEntries} />
+          <Leaderboard entries={homieEntries} masterEntries={masterEntries} playerEntries={playerEntries} leaderboard={HOMIES} />
         </article>
         <article className="w-100p">
           <h4>Who kills the Night King?</h4>
-          <img src={nightKingKiller} />
+          <img src={nightKingKiller} alt="Arya killing The Night King" />
         </article>
         <article className="w-100p">
           <h4>Who holds the Iron Throne at the end?</h4>
--- src/components/leaderboard/Leaderboard.jsx
+++ src/components/leaderboard/Leaderboard.jsx
@@ -1,9 +1,46 @@
 import React from 'react';
 
 import Entry from '../entries/Entry.jsx';
+// ? why can't my function below access imports in this global scope?
 
 function Leaderboard(props) {
-  const entryComponents = props.entries.map(entry => <Entry entry={entry} />);
+  const getCurrentMasterEntry = (props) => {
+    // current masterList is for the most recent episode
+    // ? how to leverage constants in this function so i can get the episode list
+    return props.masterEntries[props.masterEntries.length - 1]
+  }
+
+  // const calculateAlive = () => {
+
+  // }
+
+  // const calculateDead = () => {
+
+  // }
+
+  // const calculateWhiteWalker = () => {
+
+  // }
+
+  // const calculateBonuses = () => {
+
+  // }
+
+  // const scorePlayerEntry = () => {
+
+  // }
+
+  // const scorePlayerEntries = () => {
+
+  // }
+
+  // const groupAndRankPlayersByScore = () => {
+
+  // }
+
+  const entryComponents = props.entries.map(entry => <Entry entry={entry} key={entry.player} />);
+  const masterEntry = getCurrentMasterEntry(props);
+  // debugger;
 
   return (
     <div className="Leaderboard">
