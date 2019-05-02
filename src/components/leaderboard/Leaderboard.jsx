85c0faa5afa61f3b88733206414dcc4e403489cf
Bison Hubert <bisonhubert@gmail.com>
2019-05-02 09:33:40 -0700

    Remove commented code

--- src/components/leaderboard/Leaderboard.jsx
+++ src/components/leaderboard/Leaderboard.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
 import Entry from '../entries/Entry.jsx';
-// import { getCurrentMasterEntry } from '../../helpers/scoring.js'
 
 function Leaderboard(props) {
   const entryComponents = props.entries.map(entry => <Entry entry={entry} key={entry.player} />);
-  // const masterEntry = getCurrentMasterEntry(props);
-  // debugger;
 
   return (
     <div className="Leaderboard">
