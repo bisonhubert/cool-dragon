08a9374ecca56b5aa3a46d44422f72b273461362
Bison Hubert <bisonhubert@gmail.com>
2019-05-03 19:55:00 -0700

    Start styling leaderboard

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+// TODO (Bison): make dat dragon spinnn
 // TODO (Bison): add new component, Bonus.jsx
 
 import React from 'react';
@@ -7,6 +8,7 @@ import { LEADERBOARDS } from './helpers/constants.js';
 import Leaderboard from './components/leaderboard/Leaderboard';
 
 import nightKingKiller from './images/arya-night-king-2.gif'
+import coolDragon from './images/cool-dragon-2.gif'
 
 function App() {
   const leaderboardComponents = LEADERBOARDS.map((boardName, indx) => {
@@ -15,8 +17,14 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="">
+        <h1>Cool Dragon</h1>
+        <img src={coolDragon} alt="Flying dragon animation that changes colors" className="hidden"/>
+      </header>
+      <main>
         {leaderboardComponents}
+      </main>
+      <footer>
         <article className="w-100p">
           <h4>Who kills the Night King?</h4>
           <img src={nightKingKiller} alt="Arya killing The Night King" />
@@ -25,7 +33,7 @@ function App() {
           <h4>Who holds the Iron Throne at the end?</h4>
           <h5>dunno yet</h5>
         </article>
-      </header>
+      </footer>
     </div>
   );
 }
--- src/components/leaderboard/Leaderboard.jsx
+++ src/components/leaderboard/Leaderboard.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Entry from '../entries/Entry.jsx';
 
+import './Leaderboard.css';
+import '../../typography.css';
+
 import { getEntriesForLeaderboard } from '../../helpers/scoring.js'
 import { masterEntries, playerEntries } from '../entries/entryData';
 import { PRICE_PER_ENTRY } from '../../helpers/constants.js';
@@ -20,8 +23,10 @@ function Leaderboard(props) {
 
   return (
     <div className="Leaderboard">
-      <h2>{props.boardName} Leaderboard</h2>
-      <h3>Pot: ${calculatePot(sortedEntries.length, PRICE_PER_ENTRY)}</h3>
+      <header>
+        <h2 className="ta--left">{props.boardName} Leaderboard</h2>
+        <h6>Pot: ${calculatePot(sortedEntries.length, PRICE_PER_ENTRY)}</h6>
+      </header>
       {entryComponents}
     </div>
   );
