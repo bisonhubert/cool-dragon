8269ba5fac6fcdf0e8d5c3e847548307f71ed4c8
Bison Hubert <bisonhubert@gmail.com>
2019-05-07 07:52:08 -0700

    Count Cersei as sitting on Iron Throne

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@
 import React from 'react';
 import './App.css';
 
-import { LEADERBOARDS } from './helpers/constants.js';
 import Leaderboard from './components/leaderboard/Leaderboard';
 
+import { ARYA, CERSEI, LEADERBOARDS } from './helpers/constants.js';
+
 import nightKingKiller from './images/arya-night-king-2.gif'
+import ironThroneSitter from './images/cersei-throne.gif'
 import coolDragon from './images/cool-dragon-2.gif'
 
 function App() {
@@ -27,11 +29,13 @@ function App() {
       <footer>
         <article className="w-100p">
           <h4>Who kills the Night King?</h4>
+          <h5>{ARYA}</h5>
           <img src={nightKingKiller} alt="Arya killing The Night King" />
         </article>
         <article className="w-100p">
           <h4>Who holds the Iron Throne at the end?</h4>
-          <h5>dunno yet</h5>
+          <h5>Currently: {CERSEI}</h5>
+          <img src={ironThroneSitter} alt="Cersei rocking the Iron Throne" />
         </article>
       </footer>
     </div>
