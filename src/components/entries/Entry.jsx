115170727aea9131affab91d43a33686591046e6
Bison Hubert <bisonhubert@gmail.com>
2019-05-08 18:21:09 -0700

    Return entry as tbody

--- src/components/entries/Entry.jsx
+++ src/components/entries/Entry.jsx
@@ -4,19 +4,17 @@ function Entry(props) {
   const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1);
 
   return (
-    <div className="Entry">
-      <tbody>
-        <tr>
-          <td><h5 className="c--yellow">{props.entry.rank}</h5></td>
-          <td><h5 className="c--blue">{capitalizeFirstLetter(props.entry.playerName)}</h5></td>
-          <td><h5 className="c--green">{props.entry.score}</h5></td>
-          <td><a href="{props.entry.entry_doc}"
-               target="_blank"
-               rel="noopener noreferrer"><h5 className="link__c--pink">Entry</h5>
-          </a></td>
-        </tr>
-      </tbody>
-    </div>
+    <tbody className="Entry">
+      <tr>
+        <td><h5 className="c--yellow">{props.entry.rank}</h5></td>
+        <td><h5 className="c--blue">{capitalizeFirstLetter(props.entry.playerName)}</h5></td>
+        <td><h5 className="c--green">{props.entry.score}</h5></td>
+        <td><a href="{props.entry.entry_doc}"
+             target="_blank"
+             rel="noopener noreferrer"><h5 className="link__c--pink">Entry</h5>
+        </a></td>
+      </tr>
+    </tbody>
   );
 }
 
