a36dc07ba35f14ab901c1ddef750af0392c6c303
Bison Hubert <bisonhubert@gmail.com>
2019-05-08 22:24:28 -0700

    Style tablet leaderboard (#10)

--- src/App.js
+++ src/App.js
@@ -46,12 +46,16 @@ function App() {
         <article className="w--100p">
           <h4 className="c--white">Who kills the Night King?</h4>
           <h6 className="c--white">{ARYA}</h6>
-          <img src={nightKingKiller} alt="Arya killing The Night King" className="w--100p" />
+          <article className="Footer-img-container">
+            <img src={nightKingKiller} alt="Arya killing The Night King" className="w--100p" />
+          </article>
         </article>
         <article className="w--100p">
           <h4 className="c--white">Who holds the Iron Throne?</h4>
           <h6 className="c--white">{CERSEI}</h6>
-          <img src={ironThroneSitter} alt="Cersei rocking the Iron Throne" className="w--100p" />
+          <article className="Footer-img-container">
+            <img src={ironThroneSitter} alt="Cersei rocking the Iron Throne" className="w--100p" />
+          </article>
         </article>
       </footer>
     </div>
